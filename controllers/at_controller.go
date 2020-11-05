96a6339f246cae31e00cdde0b48705b81b8bef3c
Ritesh Chauhan <chauhanx.ritesh@gmail.com>
2020-11-05 12:51:21 +0530

    at resoruce update

--- controllers/at_controller.go
+++ controllers/at_controller.go
@@ -18,15 +18,19 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"strings"
 	"time"
 
 	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/ctrl"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	cnatv1alpha1 "github.com/chauhanr/cnat-operator/api/v1alpha1"
 )
@@ -43,7 +47,7 @@ type AtReconciler struct {
 
 func (r *AtReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	//todo = context.Background()
-	logger := r.Log.WithValues("at", req.NamespacedName)
+	logger := r.Log.WithValues("namespace", req.Namespace, "at", req.NamespacedName)
 	logger.Info("=== Reconciling At")
 	// Fetch the at instance
 	instance := &cnatv1alpha1.At{}
@@ -53,11 +57,76 @@ func (r *AtReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 			// could have been deleted after run therefore do not requie
 			return ctrl.Result{}, nil
 		}
-		// Error reading the object therefore requeue the request.
+		// Error reading the object therefore requeues the request.
 		return ctrl.Result{}, err
 	}
 	// your logic here
+	if instance.Status.Phase == "" {
+		// set the instance of pending if the state is blank
+		instance.Status.Phase = cnatv1alpha1.PhasePending
+	}
+	switch instance.Status.Phase {
+	case cnatv1alpha1.PhasePending:
+		logger.Info("Phase: Pending")
+		// check if it is time to act
+		logger.Info("Checking status Target " + instance.Spec.Schedule)
+		d, err := timeUntilSchedule(instance.Spec.Schedule)
+		if err != nil {
+			logger.Error(err, "Schedule parsing failure")
+			return ctrl.Result{}, err
+		}
+		logger.Info(fmt.Sprintf("Schedule passed successfully d: %v\n", d))
+		if d > 0 {
+			// not yet time therefore return
+			return ctrl.Result{RequeueAfter: d}, nil
+		}
+		logger.Info("It's time!", "Ready to execute", instance.Spec.Command)
+		instance.Status.Phase = cnatv1alpha1.PhaseRunning
+	case cnatv1alpha1.PhaseRunning:
+		logger.Info("Phase: Running")
+		pod := newPodForCR(instance)
+		err := controllerutil.SetControllerReference(instance, pod, r.Scheme)
+		if err != nil {
+			// requeues with error
+			return ctrl.Result{}, err
+		}
+		found := &corev1.Pod{}
+		nsName := types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}
+		err = r.Get(context.TODO(), nsName, found)
+		// if the pod already exists if not then create the pod in one shot
+		if err != nil && errors.IsNotFound(err) {
+			err = r.Create(context.TODO(), pod)
+			if err != nil {
+				// requeues with error
+				return ctrl.Result{}, err
+			}
+			logger.Info("Pod Launched", "name", pod.Name)
+		} else if err != nil {
+			// requeues - as error occurs so we need to requeues.
+			return ctrl.Result{}, err
+		} else if found.Status.Phase == corev1.PodFailed || found.Status.Phase == corev1.PodSucceeded {
+			logger.Info("Container terminated", "reason", found.Status.Reason, "message", found.Status.Message)
+			instance.Status.Phase = cnatv1alpha1.PhaseDone
+		} else {
+			// don't requeues as the requeues will happen as the stauts changes
+			return ctrl.Result{}, nil
+		}
+	case cnatv1alpha1.PhaseDone:
+		logger.Info("Phase Done")
+		return ctrl.Result{}, nil
+	default:
+		logger.Info("NOP")
+		return ctrl.Result{}, nil
+	}
 
+	// Update the instance, settingo the right phase
+	logger.Info(fmt.Sprintf("Updating the instance %v with status %s", instance.Name, instance.Status.Phase))
+	//r.Status().Update(ctx context.Context, obj runtime.Object, opts ...client.UpdateOption)
+	err = r.Update(context.TODO(), instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	// Don't requeues
 	return ctrl.Result{}, nil
 }
 
@@ -73,19 +142,21 @@ func newPodForCR(cr *cnatv1alpha1.At) *corev1.Pod {
 		},
 		Spec: corev1.PodSpec{
 			Containers: []corev1.Container{
-				Name:    "busybox",
-				Image:   "busybox",
-				Command: strings.Split(cr.Spec.Command, " "),
+				{
+					Name:    "busybox",
+					Image:   "busybox",
+					Command: strings.Split(cr.Spec.Command, " "),
+				},
 			},
+			RestartPolicy: corev1.RestartPolicyOnFailure,
 		},
-		RestartPolicy: corev1.RestartPolicyOnFailure,
 	}
 
 }
 
 func timeUntilSchedule(schedule string) (time.Duration, error) {
 	now := time.Now().UTC()
-	layout := "2006-01-02T15:04:02Z"
+	layout := "2006-01-02T15:04:00Z"
 	s, err := time.Parse(layout, schedule)
 	if err != nil {
 		return time.Duration(0), err
